17dfeb98f62edaa3e83a148f9e9835ab8c713cce
Rupayan Nandi <rupayannandi8055@gmail.com>
2026-08-23 22:28:58 +0530

    some changes

--- packages/db/prisma/seed.ts
+++ packages/db/prisma/seed.ts
@@ -3,15 +3,37 @@ import { prismaSingleton } from "../src/index.js";
 
 async function main() {
   logger.info("Seeding database...");
-  await prismaSingleton.user.create({
-    data: {
+  await prismaSingleton.user.upsert({
+    where: {
+      email: "abcd@a.com"
+    },
+    update: {
+      
+      name: "John Doe",
+      password: "password",
+    },
+    create: {
       email: "abcd@a.com",
       name: "John Doe",
       password: "password",
     },
   });
-  await prismaSingleton.post.create({
-    data: {
+  await prismaSingleton.post.upsert({
+    where: {
+      slug: "hello-world",
+    },
+    update: {
+      title: "Hello world",
+      content: "This is my first post",
+      published: true,
+      author: {
+        connect: {
+          email: "abcd@a.com",
+        },
+      },
+    },
+    create: {
+      slug: "hello-world",
       title: "Hello world",
       content: "This is my first post",
       published: true,
